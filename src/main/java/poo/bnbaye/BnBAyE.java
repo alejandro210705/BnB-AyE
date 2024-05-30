74ec9cf31169fb2c46802797f53c9225276b45c6
Alejandro <Alejandro@DESKTOP-ML9ECG8>
2024-05-30 11:20:19 +0200

    primer commit

---
+++ src/main/java/poo/bnbaye/BnBAyE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.bnbaye;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class BnBAyE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
