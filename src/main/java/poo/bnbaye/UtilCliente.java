453bc1708ba21bffa8eb221de2a4e1cc80cf0f3e
Alejandro <Alejandro@DESKTOP-ML9ECG8>
2024-06-02 11:46:44 +0200

    util clientes

--- src/main/java/poo/bnbaye/UtilCliente.java
+++ src/main/java/poo/bnbaye/UtilCliente.java
@@ -413,8 +413,6 @@ public class UtilCliente implements Serializable {
     }
     
     
-    
-    
     /** Consulta los datos de un anfitrion por su correo
      * @param correo
      * @return objanfi */
@@ -600,14 +598,6 @@ public class UtilCliente implements Serializable {
         }
     }//fin cargarDatos
 
-       public static Anfitrion obtenerAnfitrion(String correo, String clave){
-        for (Anfitrion anfitrion : anfitriones) {
-            if (anfitrion.getCorreo().equals(correo) && anfitrion.getClave().equals(clave)){
-                return anfitrion;
-            }
-        }
-        return null;
-    }
     
 
     
